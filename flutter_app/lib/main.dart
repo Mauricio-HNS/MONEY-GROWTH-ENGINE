751cb1a1ea419075f6b327de99317df8c1647a72
Mauricio-HNS <90620341+Mauricio-HNS@users.noreply.github.com>
2026-09-25 11:50:40 +0200

    feat: bootstrap Flutter application

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'routes.dart';
+import 'style/app_style.dart';
+
+void main() {
+  runApp(const MoneyGrowthApp());
+}
+
+class MoneyGrowthApp extends StatelessWidget {
+  const MoneyGrowthApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Money Growth Engine',
+      debugShowCheckedModeBanner: false,
+      theme: AppStyle.theme(),
+      initialRoute: AppRoutes.home,
+      onGenerateRoute: AppRoutes.onGenerateRoute,
+    );
+  }
+}
