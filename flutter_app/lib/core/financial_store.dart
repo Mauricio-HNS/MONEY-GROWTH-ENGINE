924b4ef4feda7b10e59282350dcaab5b75724f07
Mauricio-HNS <90620341+Mauricio-HNS@users.noreply.github.com>
2026-09-26 09:14:27 +0200

    feat: unify financial state and opportunity data

--- flutter_app/lib/core/financial_store.dart
+++ flutter_app/lib/core/financial_store.dart
@@ -41,6 +41,24 @@ class FinancialStore extends ChangeNotifier {
   double assets = 0;
   bool setupCompleted = false;
 
+  final List<Investment> investments = [];
+  final List<PortfolioPosition> positions = [];
+  final List<BusinessOpportunity> businessOpportunities = [];
+  final List<AssetOpportunity> assets = [];
+  final List<TaxOpportunity> taxOpportunities = [];
+
+  double get totalInvestments => investments.fold(0, (sum, item) => sum + item.amount);
+  double get portfolioValue => positions.fold(0, (sum, item) => sum + item.value);
+  double get businessPotential => businessOpportunities.fold(0, (sum, item) => sum + item.monthlyPotential);
+  double get assetPotential => assets.fold(0, (sum, item) => sum + item.monthlyPotential);
+  double get taxPotential => taxOpportunities.fold(0, (sum, item) => sum + item.estimatedValue);
+
+  void addInvestment(Investment item) { investments.add(item); notifyListeners(); }
+  void addPosition(PortfolioPosition item) { positions.add(item); notifyListeners(); }
+  void addBusiness(BusinessOpportunity item) { businessOpportunities.add(item); notifyListeners(); }
+  void addAsset(AssetOpportunity item) { assets.add(item); notifyListeners(); }
+  void addTax(TaxOpportunity item) { taxOpportunities.add(item); notifyListeners(); }
+
   double get totalIncome => incomes.fold(0, (sum, item) => sum + item.amount);
   double get totalExpenses => expenses.fold(0, (sum, item) => sum + item.amount);
   double get cashFlow => totalIncome - totalExpenses;
@@ -94,4 +112,36 @@ String money(double value, [String currency = 'EUR']) {
     _ => '€',
   };
   return symbol + ' ' + value.toStringAsFixed(0);
+}
+
+class Investment {
+  Investment({required this.name, required this.amount, required this.type});
+  final String name;
+  final double amount;
+  final String type;
+}
+
+class PortfolioPosition {
+  PortfolioPosition({required this.name, required this.value, required this.allocation});
+  final String name;
+  final double value;
+  final double allocation;
+}
+
+class BusinessOpportunity {
+  BusinessOpportunity({required this.name, required this.monthlyPotential});
+  final String name;
+  final double monthlyPotential;
+}
+
+class AssetOpportunity {
+  AssetOpportunity({required this.name, required this.monthlyPotential});
+  final String name;
+  final double monthlyPotential;
+}
+
+class TaxOpportunity {
+  TaxOpportunity({required this.name, required this.estimatedValue});
+  final String name;
+  final double estimatedValue;
 }
